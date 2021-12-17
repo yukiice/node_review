95e29444063b7088129ac81eb95a3ca506b276fc
yukiice <58852690+yukiice@users.noreply.github.com>
2021-12-17 22:55:59 +0800

    动态引进路由

--- Koa_admin/app/index.js
+++ Koa_admin/app/index.js
@@ -1,17 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const userRouter = require('../router/user.router');
 const errorHandle = require("./error-handle");
-const authRouter = require("../router/auth.router");
-
+const useRouters = require('../router/index')
 const app = new Koa()
+app.useRouters = useRouters
 // 处理数据
 app.use(bodyParser())
     // 路由处理
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
+app.useRouters()
 app.on('error',errorHandle);
 //错误处理注册
 module.exports = app
